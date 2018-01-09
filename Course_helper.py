a2db700ccecd701133ee1ffbf5a8d55907be2f9a
unknown <10751583@qq.com>
2018-01-09 20:43:38 +0800

    Update v1.2

--- Course_helper.py
+++ Course_helper.py
@@ -1,40 +1,42 @@
 import requests
 from bs4 import BeautifulSoup
-from time import ctime,sleep
+from time import ctime, sleep
 
 msgs = [
-        '提交成功',
-        '非法操作! 数据库没有对应的教学班号。',
-        '当前不在此课程类别的选课时间范围内！',
-        '您不在该教学班的修读对象范围内，不允许选此教学班！',
-        '您所在的学生群体，在此阶段不允许对该课程类别的课进行选课、退课！',
-        '系统中没有您这个学期的报到记录，不允许选课。请联系您所在院系的教务员申请补注册。',
-        '您这个学期未完成评教任务，不允许选课。',
-        '您不满足该教学班选课的性别要求，不能选此门课程！',
-        '不允许跨校区选课！',
-        '此课程已选，不能重复选择！',
-        '您所选课程 的成绩为“已通过”，因此不允许再选该课，请重新选择！',
-        '此类型课程已选学分总数超标',
-        '此类型课程已选门数超标',
-        '毕业班学生，公选学分已满，最后一个学期不允许选择公选课！',
-        '您不是博雅班学生，不能选此门课程！',
-        '您最多能选2门博雅班课程！',
-        '您不是基础实验班学生，不能选此门课程！',
-        '所选课程与已选课程上课时间冲突,请重新选择!',
-        '已经超出限选人数，请选择别的课程！',
-        '该教学班不参加选课，你不能选此教学班！',
-        '选课等待超时',
-        '您这个学期未完成缴费，不允许选课。请联系财务处帮助台（84036866 再按 3）',
-        '您未满足选择该课程的先修课程条件!',
-        '不在此课程类型的选课时间范围内',
-        '您的核心通识课学分已满足培养方案的学分要求，无法再选择核心通识课',
-        '您的主修必专绩点未达到精英课的选课要求',
-        '您已选可互认课程的同组课程',
-        '及格重修选课只能选已通过的课程',
-        '您不在教学班撤消后抢选的学生名单中',
-        '您不是卓越班学生，不能选此门课程！',
-        '早前的选课不允许退课！'
-        ]
+    '提交成功',
+    '非法操作! 数据库没有对应的教学班号。',
+    '当前不在此课程类别的选课时间范围内！',
+    '您不在该教学班的修读对象范围内，不允许选此教学班！',
+    '您所在的学生群体，在此阶段不允许对该课程类别的课进行选课、退课！',
+    '系统中没有您这个学期的报到记录，不允许选课。请联系您所在院系的教务员申请补注册。',
+    '您这个学期未完成评教任务，不允许选课。',
+    '您不满足该教学班选课的性别要求，不能选此门课程！',
+    '不允许跨校区选课！',
+    '此课程已选，不能重复选择！',
+    '您所选课程 的成绩为“已通过”，因此不允许再选该课，请重新选择！',
+    '此类型课程已选学分总数超标',
+    '此类型课程已选门数超标',
+    '毕业班学生，公选学分已满，最后一个学期不允许选择公选课！',
+    '您不是博雅班学生，不能选此门课程！',
+    '您最多能选2门博雅班课程！',
+    '您不是基础实验班学生，不能选此门课程！',
+    '所选课程与已选课程上课时间冲突,请重新选择!',
+    '已经超出限选人数，请选择别的课程！',
+    '该教学班不参加选课，你不能选此教学班！',
+    '选课等待超时',
+    '您这个学期未完成缴费，不允许选课。请联系财务处帮助台（84036866 再按 3）',
+    '您未满足选择该课程的先修课程条件!',
+    '不在此课程类型的选课时间范围内',
+    '您的核心通识课学分已满足培养方案的学分要求，无法再选择核心通识课',
+    '您的主修必专绩点未达到精英课的选课要求',
+    '您已选可互认课程的同组课程',
+    '及格重修选课只能选已通过的课程',
+    '您不在教学班撤消后抢选的学生名单中',
+    '您不是卓越班学生，不能选此门课程！',
+    '早前的选课不允许退课！'
+]
+
+
 class CourseSystem:
     __DataHandlerObj = ""
     __FoundCourse = ""
@@ -58,19 +60,19 @@ class CourseSystem:
             print(info)
 
     # 打印可选课程以及选课情况
-    def PrintAllCourse(self, choose,removeconflict=0,sort_name="None",isreverse=False):
+    def PrintAllCourse(self, choose, removeconflict=0, sort_name="None", isreverse=False):
         all_course_data = self.__DataHandlerObj.GetAllCourse(choose)
         print("已选课程：")
         for data in all_course_data[0]:
             print(data)
         print("提供课程：")
-        if sort_name=="None" :
+        if sort_name == "None":
             for data in all_course_data[1]:
-                if (not removeconflict) or data[9]!='forbid_elect':
+                if (not removeconflict) or data[9] != 'forbid_elect':
                     print(data)
         else:
-            for data in sorted(all_course_data[1],key=lambda course:course[sort_name],reverse=isreverse):
-                if (not removeconflict) or data[9]!='forbid_elect':
+            for data in sorted(all_course_data[1], key=lambda course: course[sort_name], reverse=isreverse):
+                if (not removeconflict) or data[9] != 'forbid_elect':
                     print(data)
 
     # 选课
@@ -91,6 +93,7 @@ class CourseSystem:
         print("查找结束,总共找到" + str(len(self.__FoundCourse)) + "个结果")
         return len(self.__FoundCourse)
 
+
 class DataHandler:
     __logpage = ""
     __logData = ""
@@ -108,17 +111,25 @@ class DataHandler:
         self.__logData = {
             "username": username,
             "password": password,
-            "lt": log_page_soup.find_all('input')[2]['value'],
-            "execution": log_page_soup.find_all('input')[3]['value'],
+            "captcha": None,
+            "lt": log_page_soup.find_all('input')[3]['value'],
+            "execution": log_page_soup.find_all('input')[4]['value'],
             "_eventId": "submit",
             "submit": "登陆"
         }
 
     # 登陆
     def Log(self):
+        url = "https://cas.sysu.edu.cn/cas/captcha.jsp"
+        r = self.__main_session.get(url, stream=True)
+        with open('veryfy.jpg', 'wb') as fd:
+            fd.write(r.content)
+        print(">>>请输入验证码(图片在程序目录下)")
+        captcha = input()
+        self.__logData['captcha'] = captcha
         self.__main_page = self.__main_session.post(url=self.__log_page.url, data=self.__logData)
         self.__main_page_soup = BeautifulSoup(self.__main_page.text, 'html.parser')
-        if  'Connection' in self.__main_page.headers :
+        if 'Connection' in self.__main_page.headers:
             raise Exception("登陆失败,请检查用户名和密码！")
         else:
             allxkj = self.__main_page_soup.findAll('a', href=True)
@@ -126,6 +137,7 @@ class DataHandler:
             self.__xkjdsizd = [allxkj[0]['href'][17:30], allxkj[1]['href'][17:30], allxkj[2]['href'][17:30],
                                allxkj[3]['href'][17:30]]
         return True
+
     # 返回一个列表包含用户信息
     def Getuserinfo(self):
         result_page = self.__main_session.get(
@@ -175,7 +187,7 @@ class DataHandler:
         try:
             for info in BeautifulSoup(all_course_page.text, 'html.parser').findAll('tbody')[1].findAll('tr'):
                 info_text = info.findAll('td')
-                if  info_text[0].text.find('选课') >= 1:
+                if info_text[0].text.find('选课') >= 1:
                     unelect.append(
                         [info_text[1].string, info_text[2].string.strip(), info_text[3].string, info_text[4].string,
                          info_text[5].string, info_text[6].string, info_text[7].string, info_text[8].string.strip(),
@@ -219,23 +231,26 @@ class DataHandler:
                             objlist.append(course)
         return objlist
 
+
 def log():
     global COURSE_HELPER
     print(">>>双鸭山选课系统v1.0")
     print(">>>输入数字选取相应功能")
     print(">>>1.登陆 2.退出")
-    user_input=int(input())
-    if user_input==2:
+    user_input = int(input())
+    if user_input == 2:
         exit()
     print(">>>请输入账号")
-    username=input()
+    username = input()
     print(">>>请输入密码")
-    password=input()
+    password = input()
     print(">>>正在登陆，请稍候...")
-    COURSE_HELPER=CourseSystem(username,password)
+    COURSE_HELPER = CourseSystem(username, password)
     COURSE_HELPER.Log()
     print("登陆成功\n用户信息如下:")
     COURSE_HELPER.Printuserinfo()
+
+
 def execute():
     global COURSE_HELPER
     print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
@@ -246,55 +261,56 @@ def execute():
     print(">>>4.查找相关课程")
     print(">>>5.监控选课")
     print(">>>6.退出")
-    user_input=int(input())
-    if user_input==6:
+    user_input = int(input())
+    if user_input == 6:
         exit()
-    elif user_input==1:
+    elif user_input == 1:
         COURSE_HELPER.PrintFoundCourse(input(">>>请输入课程关键字（支持模糊搜索）\n"))
-        index_choose=int(input(">>>请输入需要执行的操作 1.选课 2.退选\n"))
-        pos_choose=int(input(">>>请输入目标课程序号（列表第一个则输入1）\n"))-1
-        status=-1
+        index_choose = int(input(">>>请输入需要执行的操作 1.选课 2.退选\n"))
+        pos_choose = int(input(">>>请输入目标课程序号（列表第一个则输入1）\n")) - 1
+        status = -1
         global msgs
         times = 0
-        if index_choose==1:
-            while status!=0 and times<=10:
-                status=COURSE_HELPER.ChooseCourseFeedBack(pos_choose,'elect')
+        if index_choose == 1:
+            while status != 0 and times <= 10:
+                status = COURSE_HELPER.ChooseCourseFeedBack(pos_choose, 'elect')
                 print(msgs[status])
-                times+=1
+                times += 1
                 sleep(1)
         else:
-            while status!=0 and times<=10:
+            while status != 0 and times <= 10:
                 status = COURSE_HELPER.ChooseCourseFeedBack(pos_choose, 'unelect')
                 print(msgs[status])
-                times+=1
+                times += 1
                 sleep(1)
         print(">>>操作完成")
-    elif user_input==2:
+    elif user_input == 2:
         COURSE_HELPER.PrintCourseRes()
-    elif user_input==3:
+    elif user_input == 3:
         print(">>>输入数字获取对应功能，输入help获取对应帮助")
         class_input = input()
-        if class_input=="help":
-            print(">>>第一个数字：1.公必 2.专必 3.专选 4.公选")
+        if class_input == "help":
+            print(">>>第一个数字：1.专选 2.专必 3.公选 4.公必")
             print(">>>第二个数字：0.不排除冲突 1.排除冲突")
             print(">>>第三个数字：0.按空位排序 1.按选中率排列")
             print(">>>第四个数字：0.按升序排列 1.按降序排列")
             print(">>>默认情况下不排除冲突不进行排序")
             print(">>>例：输入1 1 2 3代表输出公必中不冲突的课程，并且按照空位升位排序")
         else:
-            input_set=class_input.split()
-            if(len(input_set)==4):
-                COURSE_HELPER.PrintAllCourse(int(input_set[0])-1,int(input_set[1]),int(input_set[2])+7,int(input_set[3]))
-            elif(len(input_set)==2):
+            input_set = class_input.split()
+            if len(input_set) == 4:
+                COURSE_HELPER.PrintAllCourse(int(input_set[0]) - 1, int(input_set[1]), int(input_set[2]) + 7,
+                                             int(input_set[3]))
+            elif len(input_set) == 2:
                 COURSE_HELPER.PrintAllCourse(int(input_set[0]) - 1, int(input_set[1]))
             else:
                 COURSE_HELPER.PrintAllCourse(int(input_set[0]) - 1)
-    elif user_input==4:
+    elif user_input == 4:
         COURSE_HELPER.PrintFoundCourse(input(">>>请输入课程关键字（支持模糊搜索）\n"))
-    elif user_input==5:
-        obj_course=input(">>>请输入课程关键字（支持模糊搜索）\n")
-        interval=int(input(">>>请设置间隔时间"))
-        while COURSE_HELPER.PrintFoundCourse(obj_course)==0:
+    elif user_input == 5:
+        obj_course = input(">>>请输入课程关键字（支持模糊搜索）\n")
+        interval = int(input(">>>请设置间隔时间"))
+        while COURSE_HELPER.PrintFoundCourse(obj_course) == 0:
             sleep(interval)
         print(">>>目标课程发现")
         status = -1
@@ -304,8 +320,10 @@ def execute():
             print(msgs[status])
             times += 1
             sleep(1)
+
+
 if __name__ == "__main__":
-    COURSE_HELPER=CourseSystem()
+    COURSE_HELPER = CourseSystem()
     log()
     while True:
         execute()
--- README.md
+++ README.md
@@ -1,11 +1,27 @@
-﻿
+
 # SYSU Course Helper
 
 ![][1] ![][2] ![][3] ![][4] ![][5]
 
 A tool aiming to help the student in Sun Yat-Sen University in Chinese mainland elect course more efficiently,is written by Python.It offers functions including checking courses,electing and unelecting courses,seeking courses by keyword and so on.Besides,you can easily get the data by import the py,and develop what you want based on it.The project provides the source code and the executable file packed by pyinstaller.You can just download the executable file for use.
 
+## 中文指引
+
+- 尚未更新打包二进制文件，需要的同学请自行根据要求配置环境
+- 相关功能尚未完善，有问题请[submit issue][9]
+- 将会在寒假期间进行大规模更新，欢迎Star
+
+## Update log
+
+- v 1.2(2018.1.9)
+  - Amendments to the elective system update, adding CAPTCHA mechanism
+- v 1.1(2017.7.20)
+  - Fix the bug: An error is reported when there is no class in a category
+- v 1.0(2017.7.14)
+  - The first version release, to achieve the basic functions
+
 ## Requirement
+
 You can use it without any reqirement by executable file,but if you'd like to execute the source code,you need these:
 
 - Python environment(3.6.0 and above)
@@ -33,6 +49,7 @@ You can `import Course_helper` or `From Course_helper import DataHandler` in you
     ```python
         obj=DataHandler(self,username,password)
     ```
+
 - Log the system,will return True,if log fails it will raise an exception
 
     ```python
@@ -45,6 +62,7 @@ You can `import Course_helper` or `From Course_helper import DataHandler` in you
         ret=obj.Getuserinfo(self)
         ret=['name','sunyatsen','id','1234']
     ```
+
 - Get user's elected course,return a list include many courses data:
 
     ```python
@@ -65,6 +83,7 @@ You can `import Course_helper` or `From Course_helper import DataHandler` in you
         res=objFindCourse(self, keyword)
         res=[[course1,teacher1],[course2,teacher2]]#Some element are ignore
     ```
+
 - Send post to system to elect/unelect course,requires a course data(a list) and the operation(a string as 'elect' or 'unelect') as parameter,return the result code as an integer
 
     ```python
@@ -72,7 +91,14 @@ You can `import Course_helper` or `From Course_helper import DataHandler` in you
     ```
 
 - You can `From Course_helper import msgs` to check the result code,which is a list,and result code means sucess when it's 0
+
 - The length of a course data is 11(user's elected course data is 10),and the tenth element contains the data which will post to the system.And the others you can print them to check what they means.
+
+## Todo
+
+- Code refactoring
+- Exception handling mechanism
+- Graphical interface
 
 ## Discussing
 
@@ -84,13 +110,13 @@ You can `import Course_helper` or `From Course_helper import DataHandler` in you
 - [My BLOG][10]
 
 
-  [1]: https://img.shields.io/badge/build-passing-brightgreen.svg
-  [2]: https://img.shields.io/badge/python-3.6.0-blue.svg
-  [3]: https://img.shields.io/badge/requests-2.18.1-green.svg
-  [4]: https://img.shields.io/badge/Beautifulsoup-4.6.0-orange.svg
-  [5]: https://img.shields.io/badge/release-1.0-red.svg
-  [6]: https://www.python.org/downloads/
-  [7]: http://www.python-requests.org/en/master/
-  [8]: https://www.crummy.com/software/BeautifulSoup/
-  [9]: https://github.com/xiejiangzhao/SYSU_course_helper/issues/new
-  [10]: http://www.xiejiangzhao.top/
+[1]: https://img.shields.io/badge/build-passing-brightgreen.svg
+[2]: https://img.shields.io/badge/python-3.6.0-blue.svg
+[3]: https://img.shields.io/badge/requests-2.18.1-green.svg
+[4]: https://img.shields.io/badge/Beautifulsoup-4.6.0-orange.svg
+[5]: https://img.shields.io/badge/release-1.0-red.svg
+[6]: https://www.python.org/downloads/
+[7]: http://www.python-requests.org/en/master/
+[8]: https://www.crummy.com/software/BeautifulSoup/
+[9]: https://github.com/xiejiangzhao/SYSU_course_helper/issues/new
+[10]: http://www.xiejiangzhao.top/
